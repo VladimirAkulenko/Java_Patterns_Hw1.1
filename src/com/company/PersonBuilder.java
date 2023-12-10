f3394cde3cecbb1e61a727d2550ea2e22724c6b5
Vladimir Akulenko <vladimirakulenk@yandex.ru>
2023-12-10 18:19:17 +0300

    Initial commit

---
+++ src/com/company/PersonBuilder.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class PersonBuilder {
+
+    private String name;
+    private String surname;
+    private int age;
+    private String address;
+
+    public PersonBuilder setName(String name) throws IllegalArgumentException {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Имя не указано");
+        } else this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) throws IllegalArgumentException {
+        if (surname == null || surname.isEmpty()) {
+            throw new IllegalArgumentException("Фамилия не указана");
+        } else this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) throws IllegalArgumentException {
+        if (age < 0) {
+            throw new IllegalArgumentException("Возраст не может быть меньше нуля");
+        } else this.age = age;
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Person build() throws IllegalArgumentException {
+        Person person;
+        if (name == null || surname == null) {
+            throw new IllegalArgumentException("Не указано имя или фамилия");
+        }
+        if (age < 0) {
+            person = new Person(name, surname);
+        } else {
+            person = new Person(name, surname, age);
+        }
+        person.setAddress(address);
+        return person;
+    }
+}
